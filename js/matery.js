b285e5fcdd14778f3e844e628bf3223f40585695
TrafalgarSX <1148636359g@gmail.com>
2020-11-16 01:05:31 +0800

    Site updated: 2020-11-16 01:05:22

--- js/matery.js
+++ js/matery.js
@@ -1,11 +1,3 @@
-/*
- * @Author: guoyawen
- * @Description:    
- * @Date: 2020-11-15 14:52:27
- * @LastEditTime: 2020-11-15 23:21:12
- * @LastEditors: your name
- * @FilePath: \undefinedd:\git\TrafalgarSX\TrafalgarSX.github.io\themes\hexo-theme-matery\source\js\matery.js
- */
 $(function () {
     /**
      * 添加文章卡片hover效果.
@@ -145,8 +137,13 @@ $(function () {
 
     function showOrHideNavBg(position) {
         let showPosition = 100;
+        if (position < showPosition) {
             $nav.addClass('nav-transparent');
+            $backTop.slideUp(300);
+        } else {
+            $nav.removeClass('nav-transparent');
             $backTop.slideDown(300);
+        }
     }
 
     	
